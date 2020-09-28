ddc9371a0178c899fe31d19b47d6138de9a930df
Juan Conrado <djconrado@uninorte.edu.co>
2020-09-28 03:34:37 -0500

    Se soluciona problema de blank page al cargar una ruta de react

--- app/controllers/react_controller.rb
+++ app/controllers/react_controller.rb
@@ -1,5 +1,8 @@
 class ReactController < ApplicationController
+  include ActionController::MimeResponds
   def index
-    render :file => 'public/index.html'
+    respond_to do |format|
+      format.html { render body: Rails.root.join('public/index.html').read }
+    end
   end
 end
